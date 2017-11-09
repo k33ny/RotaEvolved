589a1468c5403e4212ca325b2ae23f53675d32c9
Martynas Petuska <martynas.petuska@gamma.co.uk>
2017-11-09 12:13:50 +0000

    DB setup
    Signed-off-by: Martynas Petuska <martynas.petuska@gamma.co.uk>

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,14 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class App
 {
 	public static void main(String[] args)
 	{
 		JFrame f = new JFrame("Hello World");
+		f.add(new JLabel("Hello"));
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.pack();
 		f.setVisible(true);
 	}
 }
