3ae6ebff11c4fdb4a72dd93aae1bdfdfde3d4440
Martynas Petuska <martynas.petuska@gamma.co.uk>
2017-11-09 10:54:14 +0000

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class App
+{
+	public static void main(String[] args)
+	{
+		JFrame f = new JFrame("Hello World");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+}
