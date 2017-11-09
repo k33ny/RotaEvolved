24cdb0d95453f351ada398f19faa080b2cfb1a47
Martynas Petuska <martynas.petuska@gamma.co.uk>
2017-11-09 19:11:38 +0000

    ..
    Signed-off-by: Martynas Petuska <martynas.petuska@gamma.co.uk>

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,16 @@
+import frontEnd.MainForm;
+
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 /**
  * Created by mpetuska on November 2017
  */
 public class App
 {
-	private JPanel mainPanel;
-	
 	public static void main(String[] args)
 	{
-		JFrame frame = new JFrame("App");
-		frame.setContentPane(new App().mainPanel);
+		JFrame frame = new JFrame("MainForm");
+		frame.setContentPane(new MainForm().getMainPanel());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
